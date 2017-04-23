fd2a7bbe7759f7193b425e939437e80664fb35f4
Cloud <kevia@triangle.host>
2017-04-23 10:47:08 -0700

    Project setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: { presets: ['es2015', 'react'] }
+        }]
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
