2fe81819d773630605079254f8e6aa86577cd852
Cloud <kevia@triangle.host>
2017-05-02 19:11:01 -0700

    Cleaning up code

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,36 +1,14 @@
 import React from 'react';
-import Chance from 'chance';
 
 class Detail extends React.Component {
 
   constructor(props) {
     super(props);
 
-    const people = [];
-
-    for (let i = 0; i < 10; i += 1) {
-      people.push({
-        name: chance.first(),
-        counrtry: chance.country({full: true})
-      });
-    }
-
-    this.state = { people };
-  }
-
-  buttonClicked() {
-
-    // kind of like Object.assign
-    this.setState({name: chance.first()});
-
   }
 
   render() {
-      return (<div>
-        {this.state.people.map((person, index) => (
-        <p key={index}>Hello, {person.name} from {person.country}!</p>
-        ))}
-        </div>);
+      return (<div></div>);
   }
 }
 
