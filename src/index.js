28e5517bc3d00f82f41a9ffc84e96d5ce787a629
Cloud <kevia@triangle.host>
2017-05-04 13:58:18 -0700

    Added Link to Browser Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { createHashHistory } from 'history';
 
 import Detail from './pages/Detail';
